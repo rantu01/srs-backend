fa6edc9d76466e56e1bf9f1f247dcd5337849975
rantu01 <rantumondal06@gmail.com>
2025-06-07 23:11:09 +0600

    corse updat e

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://service-review-system-a0858.web.app",
     credentials: true, // কুকি এক্সচেঞ্জ করার জন্য
   })
 );
